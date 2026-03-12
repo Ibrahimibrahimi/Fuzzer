2209bb72f0469535a17feaaff2c2c78f880ca32a
Ibrahimibrahimi <ibrahim.id-wahman.06@edu.uiz.ac.ma>
2026-03-12 21:43:55 +0000

    new func : show list for user to choose wordlist to work with

--- main/config.py
+++ main/config.py
@@ -7,7 +7,7 @@ if not os.path.exists(HISTORY_DIR):
     os.mkdir(HISTORY_DIR)
 
 
-LOGO ="""\
+LOGO = """\
  __________________________________________________ 
 |    ,------.                                      |
 |    |  .---',--.,--.,-----.,-----. ,---. ,--.--.  |
@@ -16,3 +16,17 @@ LOGO ="""\
 |    `--'     `----' `-----'`-----' `----'`--'     |
 |__________________________________________________|
 """
+
+
+def makeUserChooseWordlist() -> str:
+    ws = os.listdir("main/wordlist")
+    a = None
+    while a == None or a in ws:
+        print("------ Wordlists ------ ")
+        for i in range(len(ws)) :
+            print(f"[{i+1}] {ws[i]}")
+        a = int(input("choice : "))
+        if a in range(1,len(ws)+1) :
+            return ws[a]
+        print("Please choose number from the list")
+    
